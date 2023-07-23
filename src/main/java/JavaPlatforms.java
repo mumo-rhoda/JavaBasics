3c13c127a9b5683659f053adf5a29f41538fad8b
mumo-rhoda <rhodamumo117@gmail.com>
2023-07-23 22:06:41 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/JavaPlatforms.java
@@ -0,0 +1,24 @@
+public class JavaPlatforms {
+/*
+Java - developed by Sun Microsystems.
+Types of Java Applications
+1) Standalone Application - AWT and Swing are used in
+Java for creating standalone applications.
+2) Web Application-runs on the server side and creates a dynamic page is called a web application.
+Currently, Servlet, JSP, Struts, Spring, Hibernate, JSF, etc. technologies are used for creating web applications in Java.
+3) Enterprise Application - EJB (Enterprise Java Bean) is used to develop scalable, robust and secured enterprise applications in java.
+4) Mobile Application - An application which is created for mobile devices is called a mobile application. Currently, Android and Java ME are used for creating mobile applications.
+
+Java Platforms / Editions
+1) Java SE (Java Standard Edition) - It includes Java programming APIs such as java.lang, java.io, java.net, java.util, java.sql, java.math etc. It includes core topics like OOPs, String, Regex, Exception, Inner classes, Multithreading, I/O Stream, Networking, AWT, Swing, Reflection, Collection, etc.
+
+2) Java EE (Java Enterprise Edition) - It is built on top of the Java SE platform. It includes topics like Servlet, JSP, Web Services, EJB, JPA, etc.
+
+3) Java ME (Java Micro Edition) - It is a micro platform that is dedicated to mobile applications.
+
+4) JavaFX - It uses a lightweight user interface API.
+
+
+ */
+
+}
