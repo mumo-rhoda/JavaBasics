21d0c8f93666f915b1aed7df88cbb71d0159ded7
mumo-rhoda <rhodamumo117@gmail.com>
2023-07-25 00:24:26 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/dataStructures/DataStructure.java
@@ -0,0 +1,28 @@
+package dataStructures;
+
+public class DataStructure {
+    /*
+Data structure - technique of storing and organizing data in such a way that the data can be utilized in an efficient manner.
+2 types : Linear and Non-linear data structure.
+
+Linear - accessed sequentially eg- Array, Queue, Stack, LinkedList
+Stack- uses LIFO(Last In-First Out)
+      - addition of data element in a stack is a push operation and deletion pop operation
+Queue - Uses First In-First Out (enqueue) and dequeue
+Linked list: - 2 parts data element and reference to the next node in the sequence
+
+Non-Linear - not arranged in a contiguous manner.
+            Trees and Graphs.
+            Tree- non-linear DS consists of various linked nodes(Parent-child relationship)
+            Graphs - has a finite number of vertices and edges
+
+        SOLID principles:
+        Single Responsibility- one reason to change
+        Open/Closed methods,classes - extend a class behaviour without modifying it
+        Liskovs Substitution - child-parent clasess usability
+        Interface Segregation- each interface specific responsibility
+        Dependency inversion- use abstration, decouple the dependecies.
+
+
+     */
+}
---
+++ src/main/java/designPatterns/Overview.java
@@ -0,0 +1,12 @@
+package designPatterns;
+
+public class Overview {
+    /*
+    Design patterns are classified into: Creational, Structural, Behavioural patterns and J2EE
+
+    1. Creational Patterns - provide a way to create objects while hiding the creation logic.
+    2. Structural Patterns - Concept of inheritance is used to compose interfaces and define ways to compose objects into new functionalities
+    3. Behavioral - concerned with communication between objects
+
+     */
+}
